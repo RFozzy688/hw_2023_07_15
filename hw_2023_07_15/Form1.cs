70d8d0e7352c598ad6ecffd0ede8fad1524196d9
RFozzy688 <pokerfozzy@gmail.com>
2023-07-17 09:28:09 +0300

    рефакторинг методов CreateTree() и AddDirectoryOrFile(TreeNode node, string path)

--- hw_2023_07_15/Form1.cs
+++ hw_2023_07_15/Form1.cs
@@ -1,59 +1,72 @@
 using System.Runtime;
+using System.Drawing;
+using System.IO;
 
 namespace hw_2023_07_15
 {
     public partial class Form1 : Form
     {
         string _path;
-
+        ImageList _imageList;
         public Form1()
         {
             InitializeComponent();
 
             _path = @"G:\ÿ¿√\";
+            _imageList = new ImageList();
+            //treeView1.ImageList = _imageList;
 
-            CreateTree(new DirectoryInfo(_path));
+            treeView1.BeginUpdate();
+
+            CreateTree();
+
+            treeView1.EndUpdate();
         }
-        public void CreateTree(DirectoryInfo root)
+        public void CreateTree()
         {
-            FileInfo[] files = null;
-            DirectoryInfo[] subDirs = null;
-
-            subDirs = root.GetDirectories();
-
-            foreach (DirectoryInfo dirInfo in subDirs)
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
             {
-                TreeNode node = treeView1.Nodes.Add(dirInfo.Name);
+                TreeNode node = new TreeNode { Text = drive.Name + drive.VolumeLabel };
 
-                AddDirectoryOrFile(dirInfo, node);
+                AddDirectoryOrFile(node, drive.Name);
+                treeView1.Nodes.Add(node);
             }
 
-            files = root.GetFiles();
+            //files = root.GetFiles();
 
-            foreach (FileInfo file in files)
-            {
-                treeView1.Nodes.Add(file.Name);
-            }
+            //foreach (FileInfo file in files)
+            //{
+            //    //if (!_imageList.Images.ContainsKey(file.Extension))
+            //    //{
+            //    //    iconForDir = Icon.ExtractAssociatedIcon(file.FullName);
+            //    //    _imageList.Images.Add(file.Extension, iconForDir);
+            //    //}
+
+            //    //treeView1.ImageKey = file.Extension;
+
+            //    TreeNode node = new TreeNode(file.Name);
+            //    treeView1.Nodes.Add(node);
+            //}
         }
-        public void AddDirectoryOrFile(DirectoryInfo root, TreeNode node)
+        public void AddDirectoryOrFile(TreeNode node, string path)
         {
-            FileInfo[] files = null;
-            DirectoryInfo[] subDirs = null;
+            string[] dirs = Directory.GetDirectories(path);
 
-            subDirs = root.GetDirectories();
-
-            foreach(DirectoryInfo dirInfo in subDirs)
+            foreach(string dir in dirs)
             {
-                TreeNode n = node.Nodes.Add(dirInfo.Name);
+                TreeNode nodeDir = new TreeNode();
+                nodeDir.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                node.Nodes.Add(nodeDir);
 
-                AddDirectoryOrFile(dirInfo, n);
             }
 
-            files = root.GetFiles();
+            string[] files = Directory.GetFiles(path);
 
-            foreach (FileInfo file in files)
+            foreach (string file in files)
             {
-                node.Nodes.Add(file.Name);
+                TreeNode nodeFile = new TreeNode();
+                nodeFile.Text = file.Remove(0, file.LastIndexOf("\\") + 1);
+                node.Nodes.Add(nodeFile);
             }
         }
     }
